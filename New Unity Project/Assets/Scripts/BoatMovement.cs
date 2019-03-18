5529f778757ca003bfd3158503697b8d50aaed07
Hassan <hbahemia@elam.co.uk>
2019-03-18 12:48:56 +0000

    Adding Scene Final Game added movement script

---
+++ New Unity Project/Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BoatMovement : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 1000f;
+    [SerializeField] private float accellerateSpeed = 1000f;
+
+    [SerializeField] private Rigidbody rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rbody.AddTorque(0f, h * turnSpeed * Time.deltaTime, 0f);
+        rbody.AddForce(transform.forward * v * accellerateSpeed * Time.deltaTime);
+    }
+}
