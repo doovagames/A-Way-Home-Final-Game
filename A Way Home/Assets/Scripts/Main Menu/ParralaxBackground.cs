c7bfbcd5627ea614849d7d39108d125f9b6ab481
Hassan <hbahemia@elam.co.uk>
2019-03-19 15:54:11 +0000

    parallax done

--- A Way Home/Assets/Scripts/Main Menu/ParralaxBackground.cs
+++ A Way Home/Assets/Scripts/Main Menu/ParralaxBackground.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class ParralaxBackground : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
+
+    [SerializeField] private MeshRenderer _meshrenderer;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _meshrenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 offset = new Vector2(Time.time * _speed, 0);
+        float x = Time.time * _speed;
+        
+        Vector2 offset = new Vector2(x, 0);
+        
+        _meshrenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
