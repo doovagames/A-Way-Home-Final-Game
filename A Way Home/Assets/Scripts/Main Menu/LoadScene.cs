35adb0a075266cf44a413706897fe7ca5a3e122f
Hassan <hbahemia@elam.co.uk>
2019-03-20 10:45:13 +0000

    added placeholders for menu

---
+++ A Way Home/Assets/Scripts/Main Menu/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)  //This will load the specific scene that the scene is on.
+    {
+        SceneManager.LoadScene(sceneIndex); //Load scene number.
+    }
+
+    public void Quit() // Quit the game
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+    
+}
--- A Way Home/Assets/Scripts/Main Menu/ParralaxBackground.cs
+++ A Way Home/Assets/Scripts/Main Menu/ParralaxBackground.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class ParralaxBackground : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _scrollSpeed = 10f;
+    [SerializeField] private Vector2 _startPos;
 
-    [SerializeField] private MeshRenderer _meshrenderer;
-    
-    // Start is called before the first frame update
-    void Awake()
+
+    void Start()
     {
-        _meshrenderer = GetComponent<MeshRenderer>();
+        _startPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float x = Time.time * _speed;
-        
-        Vector2 offset = new Vector2(x, 0);
-        
-        _meshrenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+        float newPos = Mathf.Repeat(Time.time * _scrollSpeed, 1000);
+        transform.position = _startPos + Vector2.right * newPos;
     }
 }
