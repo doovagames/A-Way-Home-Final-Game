9de3c3e340c13c57f6a2749bf77c06a8c7961e60
Hassan <hbahemia@elam.co.uk>
2019-03-22 15:52:50 +0000

    fixing level, particle system added

---
+++ A Way Home/Assets/Scripts/Camera/SmoothCameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public Transform _playerTransform;
+
+    [SerializeField] private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)] public float _smoothFactor = 1f;
+
+    public bool _lookAtPlayer = false;
+    
+    // Use this for initialization
+    private void Start()
+    {
+        _cameraOffset = transform.position - _playerTransform.position;
+    }
+    
+    // LateUpdate is called after the update methods
+    private void LateUpdate()
+    {
+        Vector3 newPos = _playerTransform.position + _cameraOffset;
+        
+        transform.position = Vector3.Slerp(transform.position, newPos, _smoothFactor);
+
+        if (_lookAtPlayer)
+            transform.LookAt(_playerTransform);
+    }
+}
