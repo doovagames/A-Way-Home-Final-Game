675cbd3524c85c735fba8d026521fcae655ff976
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-05-08 09:38:17 +0100

    Fixing World

--- A Way Home/Assets/Scripts/Powerup/HealthPickup.cs
+++ A Way Home/Assets/Scripts/Powerup/HealthPickup.cs
@@ -9,15 +9,20 @@ public class HealthPickup : MonoBehaviour
     [SerializeField] private AudioSource source;
 
     [SerializeField] private ParticleSystem _healthPickup;
+    [SerializeField] private GameObject _HealthPickup;
     
     public int _addHealth = 15;
+
+    private void Start()
+    {
+        _healthPickup.Play();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("HealthPickup"))
         {
             source.Play();
-            _healthPickup.Play();
             Pickup(other);
             Debug.Log("collide");
         }
@@ -27,5 +32,6 @@ public class HealthPickup : MonoBehaviour
     {
         _boatHealth._curHealth += _addHealth;
         Destroy(pickup.gameObject);
+        _HealthPickup.SetActive(false);
     }
 }
