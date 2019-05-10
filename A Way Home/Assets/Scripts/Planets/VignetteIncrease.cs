1df61a2ce5eafb8b783e93445bf9eb0f6ca3a558
Hassan <hbahemia@elam.co.uk>
2019-05-10 11:55:03 +0100

    fixing world

---
+++ A Way Home/Assets/Scripts/Planets/VignetteIncrease.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class VignetteIncrease : MonoBehaviour
+{
+
+    public PostProcessingProfile _processing;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var newSettings = VignetteComponent
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
